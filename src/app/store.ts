d57d74090ca53a3e621bb8441e7927caebfdc221
mikeydgithub <ampgzo2@gmail.com>
2022-11-17 13:13:47 -0500

    create store.ts and configure store. import datareducer from dataslice. export type Appdispatch and rootestate

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,14 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import dataReducer from '../features/data/dataSlice';
 
+// define store
+// call our dispatch function to our store. this will be accessing the reducer from data.Slice.ts and updating our store.
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
-});
+    data: dataReducer
+  }
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+
--- src/features/data/dataSlice.ts
+++ src/features/data/dataSlice.ts
@@ -1,4 +1,5 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import { DataInstance, simulateFetchApi } from "./dataAPI";
 
 export interface DataState {
     loading: boolean
@@ -10,14 +11,41 @@ const initialState: DataState = {
     data: []
 }
 
+// data transfered from dataAPI.ts
+// createAsyncThunk is a payload creater
+export const fetchDataAsync = createAsyncThunk(
+    'data/fetchData',
+    // payload
+    async (): Promise<Array<DataInstance>> => {
+        const response = await simulateFetchApi()
+        return response
+    }
+)
 // createSlice creates a reducer and can mutate the state
-const dataSlice = createSlice({
+export const dataSlice = createSlice({
     name: 'data',
     initialState,
     reducers: {},
-    extraReducers: (build) => {
-    
+    // different states of the async cycle
+    extraReducers: (builder) => {
+    builder
+        // pending state
+        .addCase(fetchDataAsync.pending, (state: DataState) => {
+            state.loading = true
+        })
+        // fufilled state
+        .addCase(fetchDataAsync.fulfilled, (state: DataState, action: PayloadAction<Array<DataInstance>>) => {
+            state.loading = false
+            state.data = action.payload
+        })
+        // if the function rejects
+        .addCase(fetchDataAsync.rejected, (state: DataState) => {
+            state.loading = false;
+        })
+        
     }
 })
 
+// integrate into store.ts
+export default dataSlice.reducer
 
